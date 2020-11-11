1866658bb45d53b94c6e6667af5fc52b5cb45666
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-11-11 15:49:17 -0300

    Adiciona funcao para criacao de conjunto de dados no CKAN

--- scripts/lib/utils.R
+++ scripts/lib/utils.R
@@ -149,7 +149,7 @@ validate_resource <- function(x) {
   file.remove("_resource.json")
 }
 
-create_resource <- function(dataset_id, resource_id) {
+create_resource <- function(resource_id, dataset_id) {
   
   datapackage <- jsonlite::read_json("datapackage.json")
   
@@ -169,4 +169,25 @@ create_resource <- function(dataset_id, resource_id) {
                                 key = Sys.getenv("DADOSMG_DEV"))
   res
   
+}
+
+create_dataset <- function() {
+  datapackage <- jsonlite::read_json("datapackage.json")
+  
+  
+  res <- ckanr::package_create(name = datapackage$name,
+                        owner_org = rlist::list.filter(datapackage$contributors, "publisher" %in% role)[[1]][["organization"]],
+                        maintainer = rlist::list.filter(datapackage$contributors, "maintainer" %in% role)[[1]][["title"]],
+                        maintainer_email = rlist::list.filter(datapackage$contributors, "maintainer" %in% role)[[1]][["email"]],
+                        version = datapackage$version,
+                        tags = datapackage$keywords %>% purrr::map(~ list(name = .x)),
+                        license_id = datapackage$licenses[[1]]$name,
+                        private = as.logical(datapackage$private),
+                        package_url = datapackage$sources[[1]]$path,
+                        title = datapackage$title,
+                        notes = datapackage$description,
+                        url = Sys.getenv("DADOSMG_DEV_HOST"), 
+                        key = Sys.getenv("DADOSMG_DEV"))
+  
+  res
 }
