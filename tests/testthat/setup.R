f24985b49d62f65fa0ad06e60519eb082c55e4c8
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2021-10-28 12:19:44 -0300

    Atualiza remuneração setembro/2021

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,6 @@ library(validate); library(dplyr); library(data.table)
 
 purrr::walk(list.files(here::here("scripts", "lib"), full.names = TRUE, pattern = ".R"), source)
 
-resource_name <- "servidores-2021-08"
+resource_name <- "servidores-2021-09"
 
 dt <- read_remuneracao(here::here("data", paste0(resource_name, ".csv.gz")))
