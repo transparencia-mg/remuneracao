3c738753d7561cd7324179b68dcdf48a9e12a5f3
Silviana Faria <silviana.faria@cge.mg.gov.br>
2024-05-30 09:10:08 -0300

    Revert "Mar/2024"
    This reverts commit c07e2af6362ba76afda0fd1de3b5ac2ec142f9f2, reversing
    changes made to 2f848a23083e44703575143f7d0418ed75abe6a9.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,10 +2,6 @@ library(validate); library(dplyr); library(data.table)
 
 purrr::walk(list.files(here::here("scripts", "lib"), full.names = TRUE, pattern = ".R"), source)
 
-<<<<<<< HEAD
 resource_name <- "servidores-2024-03"
-=======
-resource_name <- "servidores-2024-02"
->>>>>>> 58f0537c1a98ed40ec2d280a96020f976964835a
 
 dt <- read_remuneracao(here::here("data", paste0(resource_name, ".csv.gz")))
