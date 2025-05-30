418dfca4e4b117b65c36bb5a38f2228c426d2bdb
Silviana Faria <silviana.faria@cge.mg.gov.br>
2025-05-30 11:14:20 -0300

    Abril 2025

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,6 @@ library(validate); library(dplyr); library(data.table)
 
 purrr::walk(list.files(here::here("scripts", "lib"), full.names = TRUE, pattern = ".R"), source)
 
-resource_name <- "servidores-2024-10"
+resource_name <- "servidores-2025-04"
 
 dt <- read_remuneracao(here::here("data", paste0(resource_name, ".csv.gz")))
